0730c8d6beed24c2052a6d8073bc1b38b52e19cf
Junior8288 <lunganidlamini16@gmail.com>
2026-04-25 16:54:54 +0200

    Created the registration process with registerUser. A clear fields functions to remove preexisting text. To login fuction to take the user to the other page after finishing with the page

--- app/src/main/java/com/example/prog7313budgettrackerapp/RegisterPage.kt
+++ app/src/main/java/com/example/prog7313budgettrackerapp/RegisterPage.kt
@@ -1,12 +1,16 @@
 package com.example.prog7313budgettrackerapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class RegisterPage : AppCompatActivity() {
 
@@ -30,10 +34,62 @@ class RegisterPage : AppCompatActivity() {
         toLoginButton = findViewById(R.id.toLoginButton)
 
         //Awaiting the database from my teammate
+
+
+        registerButton.setOnClickListener{
+            registerUser()
+        }
+
+        toLoginButton.setOnClickListener{
+            toLoginPage()
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun registerUser() {
+        val username = username.text.toString().trim()
+        val password = password.text.toString().trim()
+        val confirmPassword = confirmPassword.text.toString().trim()
+
+
+    //Creating the validation for the password
+    //checking if both fields are empty
+
+    if(username.isEmpty() || password.isEmpty()){
+        Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
+        return
+    }
+        //Checking if password matches the confirmation password
+        if (password != confirmPassword){
+            Toast.makeText(this, "Please have the passwords match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //Need the db to work
+
+
+        }
+
+    private fun toLoginPage(){
+        toLogin()
+    }
+
+    private fun clearFields(){
+        username.text.clear()
+        password.text.clear()
+        confirmPassword.text.clear()
+    }
+
+    private fun toLogin(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish() // This closes the register page so the users cannot go back
     }
 }
