a5e4d604921b29a8a0241aedea4835d95262f329
Junior8288 <lunganidlamini16@gmail.com>
2026-04-25 20:12:03 +0200

    Now that the database is created, just made there is an admin user and a way the user can enter, save they information and login

--- app/src/main/java/com/example/prog7313budgettrackerapp/RegisterPage.kt
+++ app/src/main/java/com/example/prog7313budgettrackerapp/RegisterPage.kt
@@ -1,5 +1,7 @@
 package com.example.prog7313budgettrackerapp
 
+import Data.User
+import Data.database.AppDatabase
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -21,6 +23,7 @@ class RegisterPage : AppCompatActivity() {
     private lateinit var confirmPassword: EditText
     private lateinit var registerButton: Button
     private lateinit var toLoginButton: Button
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,15 +37,19 @@ class RegisterPage : AppCompatActivity() {
         toLoginButton = findViewById(R.id.toLoginButton)
 
         //Awaiting the database from my teammate
+        db = AppDatabase.getDatabase(this)
 
+        addDefaultUser()// To ensure there is an admin way to login
 
-        registerButton.setOnClickListener{
+        registerButton.setOnClickListener {
             registerUser()
         }
 
-        toLoginButton.setOnClickListener{
+        toLoginButton.setOnClickListener {
             toLoginPage()
         }
+
+
 
 
 
@@ -59,37 +66,84 @@ class RegisterPage : AppCompatActivity() {
         val confirmPassword = confirmPassword.text.toString().trim()
 
 
-    //Creating the validation for the password
-    //checking if both fields are empty
+        //Creating the validation for the password
+        //checking if both fields are empty
 
-    if(username.isEmpty() || password.isEmpty()){
-        Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
-        return
-    }
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
+            return
+        }
         //Checking if password matches the confirmation password
-        if (password != confirmPassword){
+        if (password != confirmPassword) {
             Toast.makeText(this, "Please have the passwords match", Toast.LENGTH_SHORT).show()
             return
         }
 
         //Need the db to work
+        lifecycleScope.launch {
+            val existingUser = db.userDao().getUserByUsername(username)
 
+            if (existingUser != null) {
+                //if the user exist , show message on screen
+                runOnUiThread {
+                    Toast.makeText(
+                        this@RegisterPage,
+                        "Username already exists.",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            } else {
+                //if the user does not exist, create a new user object
+                val newUser = User(
+                    username = username,
+                    password = password
+                )
+
+                //Insert new user in the database
+                db.userDao().insertUser(newUser)
+
+                //show Success message and move to the login screen
+                runOnUiThread {
+                    Toast.makeText(
+                        this@RegisterPage,
+                        "Registration successful.",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+
+                    clearFields()
+                    toLoginPage()
+                }
+            }
 
         }
-
-    private fun toLoginPage(){
+    }
+    private fun addDefaultUser(){
+        lifecycleScope.launch {
+            //check if admin already exist
+            val existingUser = db.userDao().getUserByUsername(username = "admin")
+            if (existingUser == null){
+                db.userDao().insertUser(
+                    User(username = "admin", password = "1234")
+                )
+            }
+        }
+    }
+    private fun toLoginPage() {
         toLogin()
     }
 
-    private fun clearFields(){
+    private fun clearFields() {
         username.text.clear()
         password.text.clear()
         confirmPassword.text.clear()
     }
 
-    private fun toLogin(){
+    private fun toLogin() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish() // This closes the register page so the users cannot go back
     }
+
 }
