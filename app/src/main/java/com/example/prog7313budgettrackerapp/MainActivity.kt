f269560501a6b4983e7d0ca484f24c802b8223e5
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2026-04-26 12:35:27 +0200

    Frontend and Backend for the Login page

--- app/src/main/java/com/example/prog7313budgettrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/prog7313budgettrackerapp/MainActivity.kt
@@ -1,20 +1,90 @@
 package com.example.prog7313budgettrackerapp
 
+import Data.database.AppDatabase
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
+
+    //global declarations
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var loginbutton: Button
+    private lateinit var registerbutton: Button
+    private lateinit var db: AppDatabase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //Room database
+        db = AppDatabase.getDatabase(this)
+
+        //Typecasting
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        loginbutton = findViewById(R.id.loginbutton)
+        registerbutton = findViewById(R.id.registerbutton)
+
+        //Set click listeners
+        loginbutton.setOnClickListener {
+           loginUser()
+        }
+
+        registerbutton.setOnClickListener {
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private  fun loginUser(){
+        val username = username.text.toString().trim()
+        val password = password.text.toString().trim()
+
+        //Validation
+        if(username.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        lifecycleScope.launch {
+            //check if user exists with the entered username and password
+            val foundUser = db.userDao().loginUser(username, password)
+
+            runOnUiThread {
+                if (foundUser != null) {
+                    Toast.makeText(this@MainActivity, "Login successful", Toast.LENGTH_SHORT).show()
+
+
+                } else{
+                    Toast.makeText(this@MainActivity, "Login Failed!", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+
 }
